3dc2b879095a4d9d5a143ff75818cd70dac40c65
rtpra1234 <67580384+rtpra1234@users.noreply.github.com>
2020-07-04 15:51:03 +0530

    Add files via upload

--- rpm/routes.py
+++ rpm/routes.py
@@ -1,20 +1,29 @@
 from rpm import app
 from rpm.utils import *
 
-from flask import send_file, request
+from flask import send_file, request, render_template
 import os
 
 
 @app.route('/')
 def home():
-    limit = request.args.get('limit', 0, type=int)
-    data = getCSV(limit)
-    return data.to_html(index=False)
+    data = getDF(15)
+
+    plot = getPlot(data)
+    data = formatTime(data)
+
+    return render_template(
+        'home.html',
+        rows=data.to_dict(orient='records'),
+        cols=data.columns.values)
 
 
 @app.route('/download')
 def download_csv():
     limit = request.args.get('limit', 0, type=int)
-    data = getCSV(limit)
+    data = getDF(limit)
+    data = formatTime(data)
     data.to_csv(os.path.join(app.root_path, 'data.csv'), index=False)
-    return send_file(os.path.join(app.root_path, 'data.csv'), mimetype='application/x-csv', attachment_filename='data.csv', as_attachment=True)
+    return send_file(
+        os.path.join(app.root_path, 'data.csv'),
+        mimetype='application/x-csv')
--- rpm/utils.py
+++ rpm/utils.py
@@ -2,11 +2,14 @@ from firebase_admin import firestore
 import pandas as pd
 from datetime import datetime
 from pytz import timezone
+from matplotlib import pyplot as plt
+from matplotlib import dates as mpl_dates
+import os
 
-from rpm import db
+from rpm import db, app
 
 
-def getCSV(limit=None):
+def getDF(limit=None):
     data = db.collection('Sensor-Data')\
         .order_by('timestamp', direction=firestore.Query.DESCENDING)
 
@@ -18,8 +21,33 @@ def getCSV(limit=None):
             [d.to_dict() for d in data.stream()])\
         .sort_values(['timestamp'])
 
-    df['timestamp'] = [str(datetime
+    return df
+
+
+def formatTime(df):
+    df['timestamp'] = [datetime
                        .fromtimestamp(time, timezone('Asia/Kolkata'))
-                       .strftime("%d/%m/%Y %H:%M:%S")) for time in df.timestamp]
+                       .strftime("%d/%m/%Y %H:%M:%S") for time in df.timestamp]
 
     return df
+
+
+def getPlot(df):
+    dates = [datetime.fromtimestamp(time, timezone(
+        'Asia/Kolkata')) for time in df.timestamp]
+    date_format = mpl_dates.DateFormatter("%d/%m/%Y\n%H:%M:%S")
+
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+
+    ax[0].plot_date(dates, df.temp, ls='-', c='red')
+    ax[1].plot_date(dates, df.humidity, ls='-', c='green')
+
+    ax[0].set(ylabel='Temperature')
+    ax[1].set(ylabel='Humidity')
+
+    fig.gca().xaxis.set_major_formatter(date_format)
+    plt.setp(ax[1].get_xticklabels(), rotation=270)
+
+    fig.savefig(os.path.join(app.root_path, 'static',
+                             'plot.png'),
+                bbox_inches='tight', transparent=True)
